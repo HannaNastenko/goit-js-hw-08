20b4757db4bd92a38f8e16517c6e9b148f87b073
HannaNastenko <114861286+HannaNastenko@users.noreply.github.com>
2023-03-21 14:31:43 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,28 @@
-import Player from '@vimeo/player';
+// import Player from '@vimeo/player';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', function (data) {
-  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
-});
+// player.on('timeupdate', function (data) {
+//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+// });
 
-const currentTime = Number(localStorage.getItem(LOCALSTORAGE_KEY));
+// const currentTime = Number(localStorage.getItem(LOCALSTORAGE_KEY));
 
-player
-  .setCurrentTime(currentTime)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+// player
+//   .setCurrentTime(currentTime)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });

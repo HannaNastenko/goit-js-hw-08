283444aae5500d16c90847ff88280af933f169a2
HannaNastenko <114861286+HannaNastenko@users.noreply.github.com>
2023-03-21 17:24:59 +0100

    hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+let formData = {};
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const btnSubmit = document.querySelector('button');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormChange, 500));
+form.addEventListener('submit', onSubmit);
+
+function onFormChange(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+if (localStorage.getItem(STORAGE_KEY)) {
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(formData);
+
+  localStorage.clear(STORAGE_KEY);
+  event.target.reset();
+}
